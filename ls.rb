cfbd22e0c4c3e18adf9afa037696fc0349acc262
itggot-erik-yngvesson <erik.yngvesson@itggot.se>
2016-12-09 17:02:34 +0100

    letat efter fel men får inte upp något syntax
    antagligen på grund av att den inte gör något för tillfället.

--- ls.rb
+++ ls.rb
@@ -1,10 +1,7 @@
 def ls ()
-
   if Dir.exist? [ARGV[0]]
-    Dir.glob("*") [ARGV[0]]
+    Dir.glob [ARGV[0]]
   else
-    return "felmedelande"
+    return File.dir ("*")
   end
-end
-
-p ls ()
+  end
